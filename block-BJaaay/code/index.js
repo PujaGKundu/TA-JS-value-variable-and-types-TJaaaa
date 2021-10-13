1657c44ee1bf3f6464f9f587743beec706c560e2
PujaGKundu <pujakundu100@gmail.com>
2021-10-13 18:53:15 +0530

    Loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,27 +3,28 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
-for (i=1; i<=10; i=i+1)
+for (let i=1; i<=10; i=i+1)
 {
     console.log (i);
 }
 
 // Using `console.log` log all the evan values from 1 to 10.
-for (i=1; i<=10; i=i+1)
+for (let i=1; i<=10; i=i+1)
 { if (i % 2 === 0){
     console.log (i);
 }    
 }
 
 // Using `console.log` log all the od values from 1 to 10.
-for (i=1; i<=10; i=i+1)
+for (let i=1; i<=10; i=i+1)
 { if (i % 2 !== 0){
     console.log (i);
 }    
 }
 
 // Calculate the sum of all numbers from 1 to 10.
-for (i=0, sum =0; i<=10; i=i+1)
+let sum=0;
+for ( let i=0; i<=10; i=i+1)
 {
     sum = sum + i;
 }
