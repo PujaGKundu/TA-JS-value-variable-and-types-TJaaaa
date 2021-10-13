3ea2693f7064ffb5d5333781a20f377fc5dc8540
PujaGKundu <pujakundu100@gmail.com>
2021-10-13 21:43:09 +0530

    Level 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,65 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+let user = prompt('Enter your name: ');
+alert(`${user}`);
+
+let userName = user;
+console.log(user);
+console.log(userName);
+
+let age = 22, isLearning = true;
+
+let user = "John";
+console.log(user);
+console.log(userName);
+
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = Number(prompt('Enter first number: '));
+let numB = Number(prompt('Enter second number: '));
+let sum = numA + numB;
+alert(`Sum is ${sum}`);
+
+
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j = j + 1){
+    if(j % 2 === 0){
+        console.log(j);
+    }
+} 
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for (let i = 9; i >= 1; i = i - 1){
+    if(j % 2 !== 0){
+        console.log(i);
+    }
+} 
+
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let i = "5";
+let sum = 0;
+while(i >= 0){
+    sum = sum + i;
+    i = i - 1;
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let i = "10";
+let sum = 0;
+while(i >= 0){
+    if(i % 2 === 0){
+        sum = sum + i;
+        i = i - 1;
+    }
+}
 
 // Comparisoin
 
@@ -36,3 +79,13 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = Number(prompt('Enter first number: '));
+let num2 = Number(prompt('Enter second number: '));
+switch(true){
+    case num1 === num2: alert(true);
+    break;
+    case num1 !== num2: alert(false);
+    break;
+    default: alert(`Enter a valid value`);
+}
